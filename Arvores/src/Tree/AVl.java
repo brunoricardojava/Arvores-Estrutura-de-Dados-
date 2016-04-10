03083641bd029afc0e7009d9e2a4c5817372aa63
Derian Fernando <derian.sobral@gmail.com>
2016-04-10 17:17:09 -0300

    Update AVl.java
    Arvore 100% funcional (Assim espero)

--- Arvores/src/Tree/AVl.java
+++ Arvores/src/Tree/AVl.java
@@ -29,14 +29,19 @@ public class AVl {
 class arvoreavl {
     noavl raiz;
     int hesq=0,hdir=0,havl=0; // variaveis que representam a altura.
+    int lesq=0,ldir=0; // variaveis que vao guardar o ultimo valor colocado ao lado esquerdo e direito da raiz.
     public arvoreavl() {
         this.raiz = null;
     }
     
     int alturaesq(noavl atual,int valor){ // achar a altura de um no a esquerda dele.
         int altura=0;
-        if (atual==this.raiz && atual.esq!=null) {
+        if(raiz.dir==null || havl==0){
+            havl++;
             altura++;
+        }
+        else{
+            hdir=0;
         }
         atual=atual.esq;
         while(atual!=null){
@@ -54,13 +59,17 @@ class arvoreavl {
                 }
             }
         }
-        return 0;
+        return altura;
     }
     
     int alturadir(noavl atual,int valor){// achar a altura de um no a direita dele.
         int altura=0;
-        if (atual==this.raiz && atual.dir!=null) {
+        if(raiz.esq==null || havl==0){
+            havl++;
             altura++;
+        }
+        else{
+            hesq=0;
         }
         atual=atual.dir;
         while(atual!=null){
@@ -78,7 +87,7 @@ class arvoreavl {
                 }
             }
         }
-        return 0;
+        return altura;
     }
     
     void balanceararvore(int valor) { // funcao que pegara o necessario para fazer o balançeamento.
@@ -100,40 +109,62 @@ class arvoreavl {
             }
         } // a partir daqui os ifs irao ver qual é a forma de balanceamento a ser aplicada
         if(bisavo==null){ // caso especial
-            System.out.println("esq "+alturaesq(avo,valor));
-            System.out.println("dir "+alturadir(avo,valor));
-            if (alturaesq(avo,valor)>alturadir(avo,valor)){
-                rotacaodir(avo,pai,filho);
+            System.out.println("esq "+alturaesq(avo,lesq));
+            System.out.println("dir "+alturadir(avo,ldir));
+            if (alturaesq(avo,lesq)>alturadir(avo,ldir)){
+                if (pai.esq==null) {
+                    rotacaodupladiresp(avo,pai,filho);
+                }
+                else{
+                    rotacaodir(avo,pai,filho);
+                }
             }
             else{
-               rotacaoesq(avo,pai,filho); 
+                if (pai.dir==null) {
+                    rotacaoduplaesqesp(avo,pai,filho);
+                }
+                else{
+                    rotacaoesq(avo,pai,filho); 
+                }
             }
         }
         else{
-            System.out.println("esq "+alturaesq(bisavo,valor));
-            System.out.println("dir "+alturadir(bisavo,valor));
-            if(alturaesq(bisavo,valor)>alturadir(bisavo,valor)){
+            System.out.println("paiesq"+alturaesq(pai,lesq));
+            System.out.println("paidir"+alturadir(pai,ldir));
+            System.out.println("esq "+alturaesq(avo,lesq));
+            System.out.println("dir "+alturadir(avo,ldir));
+            if(alturaesq(bisavo,lesq)>alturadir(bisavo,ldir)){
                 tio=avo.dir;
                 tioavo=bisavo.dir;
-                if (alturaesq(avo,valor)>alturadir(avo,valor)){
+                if (alturaesq(avo,lesq)>alturadir(avo,ldir)){
                     System.out.println("rotacaodir");
                     rotacaodir(avo,pai,filho);
                 }
                 else{
-                    System.out.println("rotacaodupladir");
-                    rotacaodupladir(bisavo,avo,pai,filho,tio,tioavo); 
+                    if (alturaesq(pai,lesq)<alturadir(pai,ldir)) {
+                        System.out.println("rotacaoesq");
+                        rotacaoesq(avo,pai,filho);
+                    }else{
+                        System.out.println("rotacaodupladir");
+                        rotacaodupladir(bisavo,avo,pai,filho,tio,tioavo); 
+                    }
                 }
             }
             else{
                 tio=avo.esq;
                 tioavo=bisavo.esq;
-                if (alturadir(avo,valor)>alturaesq(avo,valor)){
+                if (alturadir(avo,ldir)>alturaesq(avo,lesq)){
                     System.out.println("rotacaoesq");
                     rotacaoesq(avo,pai,filho);
                 }
                 else{
-                    System.out.println("rotacaoduplaesq");
-                    rotacaoduplaesq(bisavo,avo,pai,filho,tio,tioavo);
+                    if (alturaesq(pai,lesq)>alturadir(pai,ldir)) {
+                        System.out.println("rotacaodir");
+                        rotacaodir(avo,pai,filho);
+                    }else{
+                        System.out.println("rotacaoduplaesq");
+                        rotacaoduplaesq(bisavo,avo,pai,filho,tio,tioavo);
+                    }
                 }
             }
         }
@@ -144,6 +175,12 @@ class arvoreavl {
         avo.valor=pai.valor;
         pai.valor=filho.valor;
         pai.esq=null;
+    }
+    
+    void rotacaodupladiresp(noavl avo,noavl pai,noavl filho){
+        avo.dir=new noavl(avo.valor,null,null);
+        avo.valor=filho.valor;
+        pai.dir=null;
     }
     
     void rotacaodupladir(noavl bisavo,noavl avo,noavl pai,noavl filho,noavl tio,noavl tioavo){
@@ -163,6 +200,12 @@ class arvoreavl {
         pai.dir=null;
     }
     
+    void rotacaoduplaesqesp(noavl avo,noavl pai,noavl filho){
+        avo.esq=new noavl(avo.valor,null,null);
+        avo.valor=filho.valor;
+        pai.esq=null;
+    }
+    
     void rotacaoduplaesq(noavl bisavo,noavl avo,noavl pai,noavl filho,noavl tio,noavl tioavo){
         tioavo.esq=new noavl(tioavo.valor,null,null);
         tioavo.dir=new noavl(tio.valor,null,null);
@@ -174,69 +217,122 @@ class arvoreavl {
     }
     
     void inserir(int valor) { //funcao que insere o numero na arvore, ela tambem ja chama a funcao de balanceamento caso seja necessario
+        noavl prev=null;
         noavl atual=this.raiz;
         if (this.raiz == null) {
             this.raiz = new noavl(valor,null,null);
-        }else if (this.raiz !=null){
+        } 
+        else{
             if (valor>atual.valor) {
-                while (atual.dir != null) {
+                while (atual!= null) {
                     if (valor>atual.valor) {
-                        atual=atual.dir;
-                    }else{
-                        atual=atual.esq;
-                    }
-                }
-                if (valor>atual.valor) {
-                    atual.dir = new noavl(valor,null,null);
-                }else{
-                    atual.esq = new noavl(valor,null,null);
-                }
-                hdir=alturadir(this.raiz,valor);// me da o valor a direita a partir da raiz
-            }else{
-                while (atual.esq != null) {
-                    if (valor>atual.valor) {
+                        prev=atual;
                         atual=atual.dir;
                     }
                     else{
+                        prev=atual;
                         atual=atual.esq;
                     }
                 }
-                if (valor>atual.valor) {
-                    atual.dir = new noavl(valor,null,null);
-                }else{
-                    atual.esq = new noavl(valor,null,null);
+                if(atual!=null){
+                    if (valor>atual.valor) {
+                        atual.dir = new noavl(valor,null,null);
+                    }else{
+                        atual.esq = new noavl(valor,null,null);
+                    }
                 }
+                else{
+                    if (valor<prev.valor) {
+                        prev.esq=new noavl(valor,null,null);
+                    }
+                    else{
+                        prev.dir=new noavl(valor,null,null);
+                    }
+                }
+                hdir=alturadir(this.raiz,valor);// me da o valor a direita a partir da raiz
+                ldir=valor;
+            }
+            else{
+                while (atual!= null) {
+                    if (valor>atual.valor) {
+                        prev=atual;
+                        atual=atual.dir;
+                    }
+                    else{
+                        prev=atual;
+                        atual=atual.esq;
+                    }
+                }
+                if(atual!=null){
+                    if (valor>atual.valor) {
+                        atual.dir = new noavl(valor,null,null);
+                    }else{
+                        atual.esq = new noavl(valor,null,null);
+                    }
+                }
+                else{
+                    if (valor<prev.valor) {
+                        prev.esq=new noavl(valor,null,null);
+                    }
+                    else{
+                        prev.dir=new noavl(valor,null,null);
+                    }
+                }    
                 hesq=alturaesq(this.raiz,valor);// me da o valor a esquerda a partir da raiz
+                lesq=valor;
             }
         }
+        System.out.println(hdir-hesq);
         if ((hdir-hesq)==2 || (hdir-hesq)==-2) {// se isso for verdade(true) isso significa que a arvore precisa ser balanceada
+            System.out.println(hdir-hesq);
             balanceararvore(valor);
             hesq=alturaesq(this.raiz,valor); // 
             hdir=alturadir(this.raiz,valor); // Recalcula o valor dos dois para dar a altura da arvore.
         }
-        havl=Math.max(hesq,hdir); //altura da arvore
+        havl+=Math.max(hesq,hdir); //altura da arvore
     }
-    
-    @Override // função para exibir a arvore (nao implementada totalmente exibe apenas as folhas da esquerda)
-    public String toString() {
-        if (this.raiz == null) {
-            return "Arvore vazia";
+    noavl pai(int valor){
+        noavl filho=this.raiz;
+        noavl pai=null;
+        while(filho!=null && filho.valor!=valor){ 
+            pai=filho;
+            if (filho.valor>valor) {
+                filho=filho.esq;
+            }
+            else{
+                filho=filho.dir;
+            }
+            if (filho!=null && filho.valor==valor){
+                return pai;
+            }
         }
-
-        StringBuilder builder = new StringBuilder("[");
-        noavl atual = this.raiz;
-        while (atual.esq != null) {
-            builder.append(atual.valor);
-            builder.append(", ");
-            atual = atual.esq;
-        }
-        builder.append(atual.valor);
-        builder.append("]");
-
-        return builder.toString();
+        return null;
     }
+   
+    void exibirarvore() {
+    	if (this.raiz == null) {
+            System.out.println("Arvore vazia");
+        }
+                int hatual=2;
+		String separator = String.valueOf("  |__");
+		System.out.println(this.raiz.valor+"("+1+")");
+		subarvore(raiz.esq,  separator,hatual);
+		subarvore(raiz.dir, separator,hatual);
+	}
+	void subarvore(noavl no, String separator,int hatual) {
+		if (no != null) {
+			noavl pai = this.pai(no.valor);
+			if (no.equals(pai.esq) == true) {
+				System.out.println(separator+no.valor+"("+(hatual++)+")"+" (ESQ)");
+			}else{
+				System.out.println(separator+no.valor+"("+(hatual++)+")"+" (DIR)");
+			}			
+			subarvore(no.esq,  "     "+separator,hatual);
+			subarvore(no.dir, "     "+separator,hatual);
+		}
+	}
 }
-
+ 
 class noavl { // classe do no da arvore (parecida com a da lista)
 
     int valor;
